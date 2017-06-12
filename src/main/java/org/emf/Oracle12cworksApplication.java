6fcf49691967dc4ca3f7536229aa7407153ce394
gopichaithanya <gopichaithanya@users.noreply.github.com>
2017-06-13 03:53:07 +0530

    oracle 12c works

---
+++ src/main/java/org/emf/Oracle12cworksApplication.java
@@ -0,0 +1,12 @@
+package org.emf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oracle12cworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oracle12cworksApplication.class, args);
+	}
+}
